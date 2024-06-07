467359cea4cc43efa5f7f6f6c489e57c67179111
dp <dipoddp@gmail.com>
2024-06-07 09:09:58 +0700

    Fix(report): refactoring and formatting

--- tgbot/misc/report_to_owners.py
+++ tgbot/misc/report_to_owners.py
@@ -1,4 +1,6 @@
+from datetime import datetime
 from typing import Any, Tuple
+
 from aiogram import Bot
 from aiogram.enums import ParseMode
 from aiogram.types import DateTime
@@ -7,90 +9,84 @@ from betterlogging import logging
 
 from tgbot.services import broadcaster
 
-
 logger = logging.getLogger(__name__)
 
 
-class ReportBuilder():
+class ReportBuilder:
     def __init__(self, data: dict[str, Any]):
-        self.date: DateTime = DateTime.today()
-        self.location: Text | None = data['location']
-        self.author: str | None = '@' + data['author']
-        self.author_name: str | None = data['author_name']
+        self.date: DateTime = datetime.today()
+        self.location: Text | None = data.get("location")
+        self.author: str | None = "@" + str(data.get("author"))
+        self.author_name: str | None = data.get("author_name")
         # Morning report
-        self.masters_quantity: dict[str, str] = data['masters_quantity'] if 'masters_quantity' in data else {'data': 'N/A'}
-        self.latecomers: str | None = data['latecomers'] if 'latecomers' in data else None
-        self.absent: str | None = data['absent'] if 'absent' in data else None
-        self.location_text: Tuple[str] = self.location.as_kwargs()['text'] if self.location else (''),
+        self.masters_quantity: dict[str, str] = data.get("masters_quantity", "N/A")
+        self.latecomers: str | None = data.get("latecomers")
+        self.absent: str | None = data.get("absent")
+        self.location_text: Tuple[str] = (
+            self.location.as_kwargs()["text"] if self.location else (""),
+        )
         # Evening report
-        self.clients_lost: dict[str, str] = data['clients_lost'] if 'clients_lost' in data else {'data': 'N/A'}
-        self.total_clients: str | None = data['total_clients'] if 'total_clients' in data else None
-        self.sbp_sum: str | None = data['sbp_sum'] if 'sbp_sum' in data else None
-        self.day_resume: str | None = data['day_resume'] if 'day_resume' in data else None
-        self.disgruntled_clients: str | None = data['disgruntled_clients'] if 'disgruntled_clients' in data else None
-        self.argues_with_masters: str | None = data['argues_with_masters'] if 'argues_with_masters' in data else None
+        self.clients_lost: dict[str, str] = data.get("clients_lost", "N/A")
+        self.total_clients: str | None = data.get("total_clients")
+        self.sbp_sum: str | None = data["sbp_sum"] if "sbp_sum" in data else None
+        self.day_resume: str | None = data.get("day_resume")
+        self.disgruntled_clients: str | None = data.get("disgruntled_clients")
+        self.argues_with_masters: str | None = data.get("argues_with_masters")
 
     def construct_morning_report(self) -> str:
         content = as_list(
-            Bold(
-                'Утренний отчет ☀️'
+            Bold("Утренний отчет ☀️"),
+            self.date.strftime("%d.%m.%y"),
+            HashTag(
+                self.location_text[0].split("\n")[0].split(": ")[1].replace(" ", "_")
             ),
-            self.date.strftime('%d.%m.%y'),
-            HashTag(self.location_text[0].split('\n')[0].split(': ')[1].replace(' ', '_')),
-            self.location.as_kwargs()['text'] if self.location else None,
+            self.location.as_kwargs()["text"] if self.location else None,
             as_section(
-                Bold('Администратор:'),
-                f'{self.author_name} ({self.author})',
-                Bold('\n\nMастеров на смене:\n'),
-                '\n'.join([
-                    f'{k} {v}' for k, v in self.masters_quantity.items()
-                ]),
-                Bold('\n\nОпоздали:\n'),
+                Bold("Администратор:"),
+                f"{self.author_name} ({self.author})",
+                Bold("\n\nMастеров на смене:\n"),
+                "\n".join([f"{k} {v}" for k, v in self.masters_quantity.items()]),
+                Bold("\n\nОпоздали:\n"),
                 self.latecomers,
-                Bold('\n\nОтсутствуют:\n'),
-                self.absent
-            )
+                Bold("\n\nОтсутствуют:\n"),
+                self.absent,
+            ),
         )
         return content.as_markdown()
 
     def construct_evening_report(self) -> str:
         content = as_list(
-            Bold(
-                'Вечерний отчет 🌙'
+            Bold("Вечерний отчет 🌙"),
+            self.date.strftime("%d.%m.%y"),
+            HashTag(
+                self.location_text[0].split("\n")[0].split(": ")[1].replace(" ", "_")
             ),
-            self.date.strftime('%d.%m.%y'),
-            HashTag(self.location_text[0].split('\n')[0].split(': ')[1].replace(' ', '_')),
-            self.location.as_kwargs()['text'] if self.location else None,
+            self.location.as_kwargs()["text"] if self.location else None,
             as_section(
-                Bold('Администратор:'),
-                f'{self.author_name} ({self.author})',
-                Bold('\n\nУпущено клиентов:\n'),
-                '\n'.join([
-                    f'{k} {v}' for k, v in self.clients_lost.items()
-                ]),
-                Bold('\n\nВсего клиентов:\n'),
+                Bold("Администратор:"),
+                f"{self.author_name} ({self.author})",
+                Bold("\n\nУпущено клиентов:\n"),
+                "\n".join([f"{k} {v}" for k, v in self.clients_lost.items()]),
+                Bold("\n\nВсего клиентов:\n"),
                 self.total_clients,
-                Bold('\n\nСумма СБП: \n'),
+                Bold("\n\nСумма СБП: \n"),
                 self.sbp_sum,
-                Bold('\n\nКак прошел день: \n'),
+                Bold("\n\nКак прошел день: \n"),
                 self.day_resume,
-                Bold('\n\nНедовольные клиенты: \n'),
+                Bold("\n\nНедовольные клиенты: \n"),
                 self.disgruntled_clients,
-                Bold('\n\nКонфликты/споры с мастерами/между мастерами: \n'),
+                Bold("\n\nКонфликты/споры с мастерами/между мастерами: \n"),
                 self.argues_with_masters,
-            )
+            ),
         )
         return content.as_markdown()
 
-async def on_report(bot: Bot | None, admin_ids: list[int | str], report: str):
 
+async def on_report(bot: Bot | None, admin_ids: list[int | str], report: str):
     try:
         logger.info(f"Sending report {report}...")
         await broadcaster.broadcast(
-            bot,
-            admin_ids,
-            report,
-            parse_mode=ParseMode.MARKDOWN_V2
+            bot, admin_ids, report, parse_mode=ParseMode.MARKDOWN_V2
         ) if bot else ...
 
     except Exception as err:
