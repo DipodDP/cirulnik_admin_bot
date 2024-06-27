2ea101b8f17a6cec86c93aeb5881573090ba6873
DipodDP <dipoddp@gmail.com>
2024-06-27 12:23:08 +0000

    fix(database): refactor of get_or_upsert_user()

--- infrastructure/database/repo/users.py
+++ infrastructure/database/repo/users.py
@@ -1,8 +1,9 @@
 from collections.abc import Sequence
 from typing import Optional
+
 from sqlalchemy import delete, insert, select, update
-from sqlalchemy.dialects.postgresql import insert as pg_insert
 from sqlalchemy.dialects.mysql import insert as my_insert
+from sqlalchemy.dialects.postgresql import insert as pg_insert
 
 from infrastructure.database.models import User
 from infrastructure.database.models.locations import Location, UserLocation
@@ -26,66 +27,62 @@ class UserRepo(BaseRepo):
         :param username: The user's username. It's an optional parameter.
         :return: User object, None if there was an error while making a transaction.
         """
-        try:
+
+        # Determine the dialect name
+        dialect_name = self.session.bind.dialect.name
+
+        # Common values to insert
+        values = {
+            "user_id": user_id,
+            "username": username,
+            "full_name": full_name,
+            "language": language,
+            # "logged_as": logged_as,
+        }
+
+        if dialect_name == "postgresql":
+            # PostgreSQL upsert statement
             insert_stmt = (
                 pg_insert(User)
-                .values(
-                    user_id=user_id,
-                    username=username,
-                    full_name=full_name,
-                    language=language,
-                    # logged_as=logged_as,
-                )
-                .on_conflict_do_update(
-                    index_elements=[User.user_id],
-                    set_=dict(
-                        username=username, full_name=full_name, language=language
-                    ),
-                )
+                .values(**values)
+                .on_conflict_do_update(index_elements=[User.user_id], set_=values)
                 .returning(User)
             )
 
             result = await self.session.execute(insert_stmt)
+            inserted_user = result.scalar_one()
 
-        except Exception as e:
-            print(e)
+        elif dialect_name == "mysql":
+            # MySQL upsert statement
             insert_stmt = (
-                my_insert(User)
-                .values(
-                    user_id=user_id,
-                    username=username,
-                    full_name=full_name,
-                    language=language,
-                    # logged_as=logged_as,
-                )
-                .on_duplicate_key_update(
-                    username=username, full_name=full_name, language=language
-                )
+                my_insert(User).values(**values).on_duplicate_key_update(**values)
             )
 
             await self.session.execute(insert_stmt)
-            # Flush the session to ensure the insert/update operation is executed
+            # Flush the self.session to ensure the insert/update operation is executed
             await self.session.flush()
             # Query the user after insertion/updation
             user_query = select(User).where(User.user_id == user_id)
             result = await self.session.execute(user_query)
+            inserted_user = result.scalar_one()
+
+        else:
+            raise ValueError(f"Unsupported database dialect: {dialect_name}")
 
         await self.session.commit()
-        scalar = result.scalar_one()
+        return inserted_user
 
-        return scalar
-
-    async def set_user_logged_as(self, telegram_id: int, logged_as: str | None):
+    async def set_user_logged_as(self, user_id: int, logged_as: str | None):
         insert_stmt = (
-            update(User).where(User.user_id == telegram_id).values(logged_as=logged_as)
+            update(User).where(User.user_id == user_id).values(logged_as=logged_as)
         )
         result = await self.session.execute(insert_stmt)
         await self.session.commit()
 
         return result.last_updated_params()
 
-    async def get_user_by_id(self, telegram_id: int):
-        select_stmt = select(User).where(User.user_id == telegram_id)
+    async def get_user_by_id(self, user_id: int):
+        select_stmt = select(User).where(User.user_id == user_id)
         result = await self.session.execute(select_stmt)
 
         return result.scalars().first()
@@ -161,16 +158,3 @@ class UserRepo(BaseRepo):
         result = await self.session.execute(select_stmt)
 
         return result.scalars().all()
-
-    # async def get_all_locations_by_user(self, user_id: int) -> Sequence[UserLocation]:
-    #     """
-    #     Retrieve all locations for a specific user, ordered by location_id.
-    #     :return: List of UserLocation objects.
-    #     """
-    #     select_stmt = (
-    #         select(UserLocation)
-    #         .where(UserLocation.user_id == user_id)
-    #         .order_by(UserLocation.location_id.asc())
-    #     )
-    #     result = await self.session.execute(select_stmt)
-    #     return result.scalars().all()
